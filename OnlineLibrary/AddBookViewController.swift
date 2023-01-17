f10107c0c862f5894f2f311acd6b227acb9eb771
iosdev <iosdev@Aleksandrs-MacBook-Pro.local>
2023-01-17 15:37:26 +0200

    Was finished for adding books

---
+++ OnlineLibrary/AddBookViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddBookViewController.swift
+//  OnlineLibrary
+//
+//  Created by iosdev on 17.01.2023.
+//
+
+import UIKit
+
+class AddBookViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
